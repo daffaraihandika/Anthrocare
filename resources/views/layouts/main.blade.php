1e3ceb4e4500b7205347be1e45d8fcf666f8a28a
alfhandre <andre.lutfiansyah.tif21@polban.ac.id>
2023-09-08 21:16:36 +0700

    to andre dev

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,29 +1,37 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <title>Hello, world!</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container-fluid">
-      <div class="row flex-nowrap">
-        @include('partials.sidebar')
-        <div class="col-md-10 content"> <!-- Tambahkan elemen ini untuk konten utama -->
-          @yield('container')
+        <div class="row flex-nowrap">
+            @include('partials.sidebar')
+            <div class="col-md-10 content">
+                <!-- Tambahkan elemen ini untuk konten utama -->
+                @yield('container')
+            </div>
         </div>
-      </div>
     </div>
 
 
-  
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
