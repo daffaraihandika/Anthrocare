adce44b246d1483f9db770ce5785a0e028c0d084
daffa <draihandika@gmail.com>
2023-09-10 12:31:55 +0700

    feat: controller pemeriksaan

--- app/Http/Controllers/pemeriksaanController.php
+++ app/Http/Controllers/pemeriksaanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Infant;
 use App\Models\Parents;
+use Carbon\Carbon;
 
 class pemeriksaanController extends Controller
 {
@@ -22,13 +23,32 @@ class pemeriksaanController extends Controller
 
     public function getInfant($id){
         $title = "Pemeriksaan";
-        $identitas_bayi = Infant::with('parents')->get();
+        $identitas_bayi = Infant::join('parents', 'infants.id_parent', '=', 'parents.id')
+        ->where('infants.id', $id)
+        ->select('infants.id' ,'infants.nama_bayi', 'infants.jenis_kelamin', 'infants.tgl_lahir_bayi', 'infants.no_akte_bayi', 'parents.nama_orangtua')
+        ->get();
 
-        return [
-            "status" => 1,
-            "data" => $identitas_bayi,
-            "msg" => "berhasill"
-        ];
+        $identitas_bayi->map(function ($bayi) {
+            $bayi->usia = $this->calculateAgeInMonths($bayi->tgl_lahir_bayi);
+            return $bayi;
+        });
+
+        return $identitas_bayi;
     }
 
+    public function calculateAgeInMonths($birthdate) {
+        $birthdate = Carbon::parse($birthdate);
+        $currentDate = Carbon::now();
+    
+        $ageInMonths = $currentDate->diffInMonths($birthdate);
+    
+        return $ageInMonths;
+    }
+
+    // method untuk tombol get
+
+    // method untuk tombol send
+
+    // method untuk tombol submit
+    
 }
