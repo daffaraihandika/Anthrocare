7e2e0aafa499edc6b84392ed22c2b9226375d146
alfhandre <andre.lutfiansyah.tif21@polban.ac.id>
2023-09-10 21:38:23 +0700

    feat : hasil pemeriksaan

---
+++ resources/views/hasil pemeriksaan/hasilPemeriksaan.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>Hasil Pemeriksaan</h1>
+
+<div class="row">
+    <div class="col">dfg</div>
+    <div class="col">asd</div>
+</div>
+@endsection
