a4c80af8f4491af266ad055ab9330e8241ab41b2
alfhandre <andre.lutfiansyah.tif21@polban.ac.id>
2023-10-14 08:23:45 +0700

    feat api pemeriksaan, orang tua

--- app/Http/Controllers/hasilPemeriksaanController.php
+++ app/Http/Controllers/hasilPemeriksaanController.php
@@ -414,4 +414,83 @@ class hasilPemeriksaanController extends Controller
     }
 
 
+    // api
+    public function indexapi(){
+        $title = 'Hasil Pemeriksaan';
+        $data_bayi = Pemeriksaan::join('infants', 'pemeriksaan.id_infant', '=', 'infants.id')
+    ->join('parents', 'infants.id_parent', '=', 'parents.id')
+    ->select(
+        'pemeriksaan.id_infant',
+        'infants.nama_bayi',
+        'infants.no_akte_bayi',
+        'infants.tgl_lahir_bayi',
+        'infants.jenis_kelamin',
+        'parents.nama_orangtua',
+        'parents.no_ktp',
+    )
+    ->distinct("pemeriksaan.id_infant")
+    ->groupBy('infants.nama_bayi')
+    ->paginate(10);
+
+       return $data_bayi;
+    }
+
+    public function getInfantapi($id){
+        $foodSuggestions = '';
+        $title = "Hasil Pemeriksaan";
+        $identitas_bayi = Infant::join('parents', 'infants.id_parent', '=', 'parents.id')
+        ->where('infants.id', $id)
+        ->select(
+            'infants.id',
+            'infants.nama_bayi', 
+            'infants.jenis_kelamin', 
+            'infants.tgl_lahir_bayi', 
+            'infants.no_akte_bayi', 
+            'parents.nama_orangtua', 
+            'parents.alamat'
+        )
+        ->get();
+
+        $identitas_bayi->map(function ($bayi) {
+            $bayi->usia = $this->calculateAgeInMonths($bayi->tgl_lahir_bayi);
+            return $bayi;
+        });
+
+        $all_inspection = Pemeriksaan::join('infants', 'pemeriksaan.id_infant', '=', 'infants.id')
+        ->where('pemeriksaan.id_infant', $id)
+        ->select(
+            'pemeriksaan.tgl_pemeriksaan', 
+            'pemeriksaan.suhu', 
+            'pemeriksaan.berat', 
+            'pemeriksaan.panjang_badan', 
+            'pemeriksaan.zscore', 
+            'pemeriksaan.kondisi'
+        )
+        ->get();
+
+        $last_inspection = Pemeriksaan::join('infants', 'pemeriksaan.id_infant', '=', 'infants.id')
+        ->where('pemeriksaan.id_infant', $id)
+        ->select(
+            'pemeriksaan.tgl_pemeriksaan', 
+            'pemeriksaan.suhu', 
+            'pemeriksaan.berat', 
+            'pemeriksaan.panjang_badan', 
+            'pemeriksaan.zscore', 
+            'pemeriksaan.kondisi',
+            'pemeriksaan.created_at',
+            'infants.usia',
+            'infants.tgl_lahir_bayi'
+        )
+        ->orderBy('pemeriksaan.created_at', 'desc')
+        ->first();
+
+        $status = [
+            'kondisi' => $last_inspection['kondisi'],
+            'food_suggestions' => $foodSuggestions
+        ];
+
+
+        return $last_inspection;
+        // return view('hasil pemeriksaan/detail', compact('title', 'identitas_bayi', 'last_inspection', 'all_inspection', 'status'));
+    }
 }
